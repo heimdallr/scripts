c4606ecdcc668733db7c5a54dca14cdeb2d54fc7
heimdallr <heimdalr@ngs.ru>
2023-07-10 16:11:25 +0700

    Скрипты

--- cmake/thirdparty/quazip.cmake
+++ cmake/thirdparty/quazip.cmake
@@ -1,1 +1,5 @@
-set(CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR} ${CMAKE_MODULE_PATH})
+set(quazip_ROOT "$ENV{THIRDPARTY}/quazip/${PLATFORM}")
+set(CMAKE_MODULE_PATH "${quazip_ROOT}/lib/cmake/QuaZip-Qt6-1.4" ${CMAKE_MODULE_PATH})
+set(CMAKE_PREFIX_PATH "${quazip_ROOT}/lib/cmake/QuaZip-Qt6-1.4" ${CMAKE_PREFIX_PATH})
+
+find_package(QuaZip-Qt6)
---
+++ cmake/thirdparty/zlib.cmake
@@ -0,0 +1,3 @@
+﻿set(zlib_ROOT "$ENV{THIRDPARTY}/zlib/${PLATFORM}")
+set(ZLIB_LIBRARY "${zlib_ROOT}/lib/zlib.lib")
+set(ZLIB_INCLUDE_DIR "${zlib_ROOT}/include")
