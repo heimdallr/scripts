b47b38f06cd4c791224d7e77f44159faf15580cb
heimdallr <heimdalr@ngs.ru>
2023-12-03 15:53:51 +0700

    7za.dll обязателен, Qt6::Xml добавлен

--- cmake/sdk/7z.cmake
+++ cmake/sdk/7z.cmake
@@ -1,4 +1,6 @@
 set(7z_ROOT "${SDK_PATH}/7z/x64")
 string(REPLACE "\\" "/" 7z_ROOT ${7z_ROOT})
-#message(FATAL_ERROR "${7z_ROOT}/bin/7za.dll -> ${CMAKE_BINARY_DIR}/bin/${CMAKE_BUILD_TYPE}")
+if (NOT EXISTS ${7z_ROOT}/bin/7za.dll)
+	message(FATAL_ERROR "7za.dll not found")
+endif()
 file(COPY ${7z_ROOT}/bin/7za.dll DESTINATION ${CMAKE_BINARY_DIR}/bin/${CMAKE_BUILD_TYPE})
--- cmake/sdk/qt.cmake
+++ cmake/sdk/qt.cmake
@@ -9,7 +9,7 @@ set(Qt6Tools_DIR $ENV{Qt6_DIR}/lib/cmake)
 set(Qt6CoreTools_DIR ${Qt6Tools_DIR}/Qt6CoreTools)
 set(Qt6GuiTools_DIR ${Qt6Tools_DIR}/Qt6GuiTools)
 
-set(QtModules Widgets)
+set(QtModules Widgets Xml)
 find_package(Qt6 REQUIRED COMPONENTS ${QtModules})
 
 set(icu_modules icudt icuio icuin icuuc)
