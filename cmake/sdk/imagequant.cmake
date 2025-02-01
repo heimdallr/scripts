50ac2a9226858a0cbc5bb181f9e3a993a11d12d6
heimdallr <heimdalr@ngs.ru>
2025-02-01 20:39:53 +0700

    add imagequant

---
+++ cmake/sdk/imagequant.cmake
@@ -0,0 +1,18 @@
+﻿set(imagequant_ROOT "${SDK_PATH}/imagequant")
+string(REPLACE "\\" "/" imagequant_ROOT ${imagequant_ROOT})
+
+set(imagequant_INCLUDE_DIR "${imagequant_ROOT}/include")
+set(imagequant_LIB_DIR "${imagequant_ROOT}/x64/lib")
+set(imagequant_BIN_DIR "${imagequant_ROOT}/x64/bin")
+
+add_library(imagequant SHARED IMPORTED)
+set_target_properties(imagequant PROPERTIES
+        # 64
+        INTERFACE_INCLUDE_DIRECTORIES ${imagequant_INCLUDE_DIR}
+        IMPORTED_IMPLIB_DEBUG ${imagequant_LIB_DIR}/imagequant.dll.lib
+        IMPORTED_IMPLIB_RELEASE ${imagequant_LIB_DIR}/imagequant.dll.lib
+        IMPORTED_LOCATION_DEBUG ${imagequant_BIN_DIR}/imagequant.dll
+        IMPORTED_LOCATION_RELEASE ${imagequant_BIN_DIR}/imagequant.dll
+)
+
+set(imagequant_FOUND TRUE)
